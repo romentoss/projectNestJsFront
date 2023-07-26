f43b3b92b51a4c551e38cf9d5c024ac06911e3ec
Romentos <padilla.naranja@gmail.com>
2023-07-26 12:54:36 +0200

    App guards for authentification

---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,21 @@
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  templateUrl: './dashboard-layout.component.html',
+  styleUrls: ['./dashboard-layout.component.css']
+})
+export class DashboardLayoutComponent {
+  private authService = inject(AuthService);
+
+  //En base a propiedades computadas
+
+  public user = computed(()=> this.authService.currentUser());
+
+  // get user(){
+  //   return this.authService.currentUser();
+  // }
+  onLogout(){
+    this.authService.logout();
+  }
+}
